c40e052cde36442ab3a289bbc6df277895d764fb
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-07-23 20:59:58 +0200

    globalStyle added

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html {
+    font-size: 62.5%;
+  }
+  
+  body {
+    font-size: 1.6rem; 
+    font-family: 'Lato', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
