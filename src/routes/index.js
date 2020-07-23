de6130bc7df8b05b17b8f67b83a4a04b52bdcdb0
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-07-23 21:12:28 +0200

    routes added

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+export const routes = {
+  home: '/home',
+  gallery: '/gallery',
+  forgot: '/forgot',
+  galleryList: '/galleryList',
+  galleryCard: '/galleryCard/:id',
+  signup: '/signup',
+  login: '/login',
+};
