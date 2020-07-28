b964a359e357130431eb6a0406498ecacfc84797
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-07-28 20:16:56 +0200

    Shared index added

---
+++ src/components/shared/index.js
@@ -0,0 +1,8 @@
+import Paragraph from 'components/shared/Paragraph';
+import Input from 'components/shared/Input';
+import Button from 'components/shared/Button';
+import Heading from 'components/shared/Heading';
+
+export {
+  Paragraph, Input, Button, Heading,
+};
