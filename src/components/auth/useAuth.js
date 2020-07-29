3e04d37889af9798f9927860e589528db51e6acc
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-07-29 19:05:22 +0200

    Initial commit

---
+++ src/components/auth/useAuth.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+import firebase from '../../firebase';
+
+const useAuth = () => {
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+  return authUser;
+};
+
+export default useAuth;
---
+++ src/components/auth/useFormValidation.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+const useFormValidation = (initialState, validate, authenticate) => {
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    const noErrors = (Object.keys(errors).length === 0 && isSubmitting);
+    if (noErrors) {
+      authenticate();
+      setSubmitting(false);
+    } else {
+      setSubmitting(false);
+    }
+  }, [errors, isSubmitting, authenticate]);
+
+  const handleChange = (event) => {
+    event.persist();
+    setValues((previousValues) => ({
+      ...previousValues,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleBlur = () => {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
+    // console.log({ values });
+  };
+
+  return {
+    handleChange, handleSubmit, handleBlur, isSubmitting, values, errors,
+  };
+};
+
+export default useFormValidation;
