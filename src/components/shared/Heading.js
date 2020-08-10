eeba6e98a2aaeee90cec0f64c1f6e6c9e9113dd1
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-08-10 23:27:25 +0200

    Shared elements updated

--- src/components/shared/Heading.js
+++ src/components/shared/Heading.js
@@ -4,6 +4,8 @@ const Heading = styled.h1`
   color: ${({ theme }) => theme.veryDarkGrey};
   font-size: ${({ theme, big }) => (big ? theme.fontSize.xl : theme.fontSize.l)};
   font-weight: ${({ theme }) => theme.bold};
+  padding: .4rem .5rem;
+  margin: 0;
 `;
 
 export default Heading;
--- src/components/shared/Paragraph.js
+++ src/components/shared/Paragraph.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const Paragraph = styled.p`
   font-size: ${({ theme }) => theme.fontSize.s};
   font-weight: ${({ theme }) => theme.fontWeight.light};
+  color: ${({ theme }) => theme.veryDarkGrey};
+  padding: .2rem .5rem;
+  margin: 0;
 `;
 
 export default Paragraph;
