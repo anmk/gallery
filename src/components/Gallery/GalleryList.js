48cae88113c36570db8e80654f21362c33b3493f
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-08-11 20:55:20 +0200

    GalleryList updated

--- src/components/Gallery/GalleryList.js
+++ src/components/Gallery/GalleryList.js
@@ -1,23 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+import { StyledGalleryWrapper } from 'components/Gallery/galleryStyled';
 import GalleryCard from 'components/Gallery/GalleryCard';
 import FirebaseContext from '../../firebase/context';
 
-const StyledWrapper = styled.div`
-  padding: 3rem;
-`;
-
-const StyledGrid = styled.div`
-  padding-top: 2rem;
-  display: flex;
+const StyledWrapper = styled(StyledGalleryWrapper)`
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: center;
-`;
-
-const StyledPageHeader = styled.div`
-  margin: 25px 0 50px 0;
+  padding: 3rem;
 `;
 
 const GalleryList = () => {
@@ -25,31 +17,24 @@ const GalleryList = () => {
   const { firebase } = useContext(FirebaseContext);
   const [galleries, setGalleries] = useState([]);
 
-  const handleSnapshot = (snapshot) => {
-    const galleryList = snapshot.docs.map((doc) => ({
-      id: doc.id, ...doc.data(),
-    }));
-    console.log(galleryList);
-    setGalleries(galleryList);
-  };
-
   useEffect(() => {
+    const handleSnapshot = (snapshot) => {
+      const galleryList = snapshot.docs.map((doc) => ({
+        gid: doc.id, ...doc.data(),
+      }));
+      setGalleries(galleryList);
+    };
     firebase.db.collection(GALLERIES_URL).onSnapshot(handleSnapshot);
   }, [firebase.db]);
 
   return (
-    <>
-      <StyledWrapper>
-        <StyledPageHeader>Gallery List</StyledPageHeader>
-        <StyledGrid>
-          {galleries.map((gallery) => (
-            <div key={gallery.id}>
-              <GalleryCard {...gallery} />
-            </div>
-          ))}
-        </StyledGrid>
-      </StyledWrapper>
-    </>
+    <StyledWrapper>
+      {galleries.map((gallery) => (
+        <div key={gallery.gid}>
+          <GalleryCard {...gallery} />
+        </div>
+      ))}
+    </StyledWrapper>
   );
 };
 
