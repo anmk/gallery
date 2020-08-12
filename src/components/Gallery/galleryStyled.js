e220b724cdbd5273733787ee1dba7708b39ca678
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-08-12 20:32:41 +0200

    galleryStyled added

---
+++ src/components/Gallery/galleryStyled.js
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+import { Heading } from 'components/shared';
+
+const StyledGalleryBorder = css`
+  border: 1px solid ${({ theme }) => theme.darkGrey};
+  border-radius: 5px;
+`;
+
+export const StyledGalleryHeading = styled(Heading)`
+  color: ${({ theme }) => theme.veryDarkGrey};
+  font-size: ${({ theme }) => (theme.fontSize.m)};
+`;
+
+export const StyledGalleryImage = styled.img`
+  max-width: 60rem;  
+  max-height: 60rem;
+  ${StyledGalleryBorder}
+  align-self: center;
+`;
+
+export const StyledGalleryWrapper = styled.div`
+  ${StyledGalleryBorder}
+  display: flex;
+  box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.5);
+  border-radius: 10px;
+  overflow: hidden;
+  margin-top: 2rem;
+`;
+
+export const StyledGalleryInnerWrapper = styled.div`
+  padding: 1rem;
+`;
