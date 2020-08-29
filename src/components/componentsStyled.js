d36ec4ee86cb5014c74e2c5031b0087bd2f33665
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-08-29 18:01:42 +0200

    componentsStyled added

---
+++ src/components/componentsStyled.js
@@ -0,0 +1,20 @@
+import styled, { css } from 'styled-components';
+
+import { Heading } from 'components/shared';
+
+export const StyledOuterContainer = styled(Heading)`
+  height: 1rem;
+`;
+
+export const StyledFormFont = css`
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  text-decoration: none;
+`;
+
+export const StyledFormError = styled.p`
+  ${StyledFormFont};
+  padding: 0;
+  margin: 0;
+  color: ${({ theme }) => theme.error}; 
+`;
