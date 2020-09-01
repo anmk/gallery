b04846e732d85f9c7d35564e9d5dd15c56355bc9
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-09-01 20:56:01 +0200

    Textarea styled added

---
+++ src/components/shared/Textarea.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+import {
+  StyledGalleryBasicElement, StyledGalleryPlaceholderElement,
+} from 'components/shared/sharedStyled';
+
+const Textarea = styled.textarea`
+  ${StyledGalleryBasicElement};
+  height: 5rem;
+  border: 1px solid ${({ theme }) => theme.darkGrey};
+  border-radius: 5px;
+  padding-top: .5rem;
+
+  ::placeholder {
+    ${StyledGalleryPlaceholderElement};
+  }
+`;
+
+export default Textarea;
