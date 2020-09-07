1869f94c1ef279c04002d69788bd7b6795b02413
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-09-07 21:20:55 +0200

    GalleryThumbnail updated

--- src/components/Gallery/GalleryThumbnail.js
+++ src/components/Gallery/GalleryThumbnail.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { Paragraph } from 'components/shared';
+import GalleryPhotoUpload from 'components/Gallery/GalleryPhotoUpload';
+import { Paragraph, Button } from 'components/shared';
 import {
   StyledGalleryWrapper, StyledGalleryInnerWrapper, StyledGalleryHeading, StyledGalleryImage,
 } from 'components/Gallery/galleryStyled';
@@ -10,38 +11,67 @@ import noImageAvailable from 'assets/images/no-image-available.svg';
 import FirebaseContext from '../../firebase/context';
 
 
-const StyledWrapper = styled(StyledGalleryWrapper)`
-  flex-flow: row wrap;
+const StyledThumbnailWrapper = styled(StyledGalleryWrapper)`
+  flex-direction: column;
+  margin: 1.5rem;
+`;
+
+export const StyledInnerThumbnailWrapper = styled(StyledGalleryInnerWrapper)`
+  display: flex;
   justify-content: center;
-  margin: 15px;
+  position: relative;
+`;
+
+export const StyledThumbnailElement = styled.div`
+  display: flex;
+  justify-content: start;
+  padding: 1rem;
+  margin-left: 2rem;
+`;
+
+const StyledUploadContainer = styled.div`
+  position: absolute;
+  top: 30px;
 `;
 
 const StyledPhoto = styled(StyledGalleryImage)`
+  width: 100%;
   margin-top: 1rem;
-  border-radius: 10px;
+  border-radius: 1rem;
+  opacity: 0.2;
 `;
 
 const GalleryThumbnail = () => {
   const COLLECTION_URL = 'galleries';
   const { gid } = useParams();
-  const { firebase } = useContext(FirebaseContext);
+  const { fbase } = useContext(FirebaseContext);
   const [gallery, setGallery] = useState([]);
 
   useEffect(() => {
-    firebase.db.doc(`${COLLECTION_URL}/${gid}`).onSnapshot((doc) => {
+    fbase.db.doc(`${COLLECTION_URL}/${gid}`).onSnapshot((doc) => {
       const galleryInfo = doc.data();
       setGallery(galleryInfo);
     });
-  }, [firebase.db, gid]);
+  }, [fbase.db, gid]);
 
   return (
-    <StyledWrapper>
-      <StyledGalleryInnerWrapper>
-        <StyledGalleryHeading>Gallery: {gallery.name}</StyledGalleryHeading>
-        <StyledPhoto src={gallery.imageUrl || noImageAvailable} alt={gallery.name} />
-        <Paragraph>{gallery.description}</Paragraph>
-      </StyledGalleryInnerWrapper>
-    </StyledWrapper>
+    <StyledThumbnailWrapper>
+      <StyledThumbnailElement>
+        <StyledGalleryHeading>Gallery: {gallery?.name}</StyledGalleryHeading>
+      </StyledThumbnailElement>
+      <StyledInnerThumbnailWrapper>
+        <StyledPhoto src={gallery?.imageUrl || noImageAvailable} alt={gallery?.name} />
+        <StyledUploadContainer>
+          <GalleryPhotoUpload />
+        </StyledUploadContainer>
+      </StyledInnerThumbnailWrapper>
+      <StyledThumbnailElement>
+        <Paragraph>{gallery?.description}</Paragraph>
+      </StyledThumbnailElement>
+      <StyledThumbnailElement>
+        <Button secondary="true" as={Link} to="../">Go to all galleries</Button>
+      </StyledThumbnailElement>
+    </StyledThumbnailWrapper>
   );
 };
 
