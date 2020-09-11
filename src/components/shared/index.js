76f4de02552017613a5e266ee5b108fb9ee166bf
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-09-11 23:28:45 +0200

    Shared index updated

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -2,8 +2,9 @@ import Paragraph from 'components/shared/Paragraph';
 import Input from 'components/shared/Input';
 import Textarea from 'components/shared/Textarea';
 import Button from 'components/shared/Button';
+import ButtonImage from 'components/shared/ButtonImage';
 import Heading from 'components/shared/Heading';
 
 export {
-  Paragraph, Input, Textarea, Button, Heading,
+  Paragraph, Input, Textarea, Button, ButtonImage, Heading,
 };
