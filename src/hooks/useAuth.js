fe9d44bbff9981729600a0db1a1f981375bfb53f
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-09-15 20:05:44 +0200

    useAuth updated

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,20 +2,30 @@ import { useEffect, useState } from 'react';
 
 import fbase from '../firebase';
 
-
 const useAuth = () => {
   const [authUser, setAuthUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = fbase.auth.onAuthStateChanged((user) => {
-      if (user) {
-        setAuthUser(user);
-      } else {
-        setAuthUser(null);
-      }
-    });
-    return () => unsubscribe();
+    let didCancel = false;
+
+    const authData = async () => {
+      const unsubscribe = fbase.auth.onAuthStateChanged((user) => {
+        if (user) {
+          setAuthUser(user);
+        } else {
+          setAuthUser(null);
+        }
+      });
+      return () => unsubscribe();
+    };
+    if (!didCancel) {
+      authData();
+    }
+    return () => {
+      didCancel = true;
+    };
   }, []);
+
   return authUser;
 };
 
