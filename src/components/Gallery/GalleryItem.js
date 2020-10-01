e4495594df34969aad84145f4c355f13d7ba50a7
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-10-01 22:21:33 +0200

    GalleryItem updated

--- src/components/Gallery/GalleryItem.js
+++ src/components/Gallery/GalleryItem.js
@@ -1,13 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { useParams, Outlet } from 'react-router-dom';
+import { useParams, Outlet, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import GalleryDetails from 'components/Gallery/GalleryDetails';
 import { Paragraph } from 'components/shared';
 import {
-  StyledGalleryWrapper, StyledGalleryHeading, StyledGalleryImage,
+  StyledGalleryWrapper, StyledGalleryHeading, StyledGalleryImage, StyledButtonImage,
 } from 'components/Gallery/galleryStyled';
 import noImageAvailable from 'assets/images/no-image-available.svg';
+import deleteImage from 'assets/images/icons8-cancel.svg';
 import AppContext from 'context';
 
 const StyledWrapper = styled(StyledGalleryWrapper)`
@@ -18,17 +19,18 @@ const StyledWrapper = styled(StyledGalleryWrapper)`
 const StyledTitle = styled.div`
   height: 90px;
   display: flex;
-  justify-content: flex-start;
-  padding-left: 1rem;
-  border: 1px solid ${({ theme }) => theme.lightGrey};
+  justify-content: space-between;
+  padding: 1rem;
+  border: 1px solid ${({ theme }) => theme.darkGrey};
   box-shadow: 0 3px 10px -3px hsla(0, 0%, 0%, 0.5);
-  border-radius: 5px;
+  border-radius: 1rem;
   align-items: center;
 `;
 
 const StyledItem = styled.div`
   width: 49%;
   padding: 1rem;
+  box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.5);
   border: 1px solid ${({ theme }) => theme.darkGrey};
   border-radius: 10px;
   margin-top: 1rem;
@@ -47,10 +49,22 @@ const StyledPhoto = styled(StyledGalleryImage)`
   max-height: 80px;
 `;
 
+const StyledButtonItemImage = styled(StyledButtonImage)`
+  border-radius: 50%;
+  background-size: 140%;
+`;
+
+const StyledTitleText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const GalleryItem = () => {
   const COLLECTION_URL = 'galleries';
   const IMAGE_URLS = 'imageUrls';
   const { gid } = useParams();
+  const navigate = useNavigate();
   const { fbase } = useContext(AppContext);
   const [info, setInfo] = useState([]);
   const [gallery, setGallery] = useState([]);
@@ -90,15 +104,35 @@ const GalleryItem = () => {
     };
   }, [fbase.db, gid]);
 
+  const handleCardDelete = async () => {
+    // console.log('Gallery: ', gallery.length);
+    // console.log('nameInStorage: ', info?.nameInStorage);
+    // console.log('name: ', info?.name);
+    if (gallery.length > 0) {
+      console.log('Remove photos from Gallery: ', gallery.length);
+    } else {
+      await fbase.storage().ref('images').child(info?.nameInStorage).delete()
+        .then(
+          console.log('Card has been deleted from storage!', ''),
+          await fbase.db.doc(`${COLLECTION_URL}/${gid}`).delete()
+            .then(() => console.log('Card data from db has been deleted!', ''))
+            .catch((error) => console.log(error.message, '')),
+        )
+        .catch((error) => console.log(error.message, ''));
+      navigate(`/${COLLECTION_URL}`);
+    }
+  };
+
   return (
     <StyledWrapper>
       <StyledItem>
         <StyledTitle>
           <StyledPhoto src={info?.imageUrl || noImageAvailable} alt={info?.name} />
-          <div>
+          <StyledTitleText>
             <StyledGalleryHeading>{info?.name}</StyledGalleryHeading>
             <Paragraph>{info?.description}</Paragraph>
-          </div>
+          </StyledTitleText>
+          <StyledButtonItemImage onClick={handleCardDelete} image={deleteImage} />
         </StyledTitle>
 
         <StyledBox>
