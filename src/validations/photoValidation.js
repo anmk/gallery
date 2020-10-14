fd2aca0e56498b2302629b2efacb1b8ceb18169c
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-10-14 23:17:36 +0200

    photoValidation updated

--- src/validations/photoValidation.js
+++ src/validations/photoValidation.js
@@ -3,8 +3,12 @@ export default function photoValidation(values) {
   // Name errors
   if (!values.name) {
     errors.name = 'Name required';
-  } else if (values.name.length < 2) {
-    errors.name = 'Name must be at least 2 characters';
+  } else if (values.name.length < 2 || values.name.length > 13) {
+    errors.name = 'Name must be between 2 and 13 characters';
+  }
+  // Description errors
+  if (values.description.length > 24) {
+    errors.description = 'Description can be up to 24 characters long';
   }
   return errors;
 }
