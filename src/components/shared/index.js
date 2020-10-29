ee2f1cdaaab621631d24e025cbcb131b8c66f20b
Andrzej Kałowski <a.m.kalowski@gmail.com>
2020-10-29 21:32:03 +0100

    index updated

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -4,7 +4,8 @@ import Textarea from 'components/shared/Textarea';
 import Button from 'components/shared/Button';
 import ButtonImage from 'components/shared/ButtonImage';
 import Heading from 'components/shared/Heading';
+import { Row, Col } from 'components/shared/responsiveGrid';
 
 export {
-  Paragraph, Input, Textarea, Button, ButtonImage, Heading,
+  Paragraph, Input, Textarea, Button, ButtonImage, Heading, Row, Col,
 };
